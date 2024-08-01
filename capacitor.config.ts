b7c8789ccf3eee4adb712af3a33fba4b58dce39f
Daniel Atilano <daniel.atilano@live.com>
2024-08-01 20:34:55 +0200

    ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.danielatilano.com',
+  appName: 'react-website-art',
+  webDir: 'build'
+};
+
+export default config;
