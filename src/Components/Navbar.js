96265b79259b41aad23efbc9c4b857012f4cc078
Daniel Atilano <daniel.atilano@live.com>
2024-08-05 16:17:40 +0200

    pinch zoom5

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
-//import { Link } from 'react-router-dom';
 import './Navbar.css';
 import myLogo from '../logo_transparent.png';
 
