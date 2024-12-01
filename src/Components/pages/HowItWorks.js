f7954dd108ceeae03d5ad97dbd765eba6736b98b
Daniel Atilano <daniel.atilano@live.com>
2024-12-01 18:06:15 +0100

    new_section

--- src/Components/pages/HowItWorks.js
+++ src/Components/pages/HowItWorks.js
@@ -12,18 +12,36 @@ export default function HowItWorks(){
         i18n.changeLanguage(lang);
     };
 
+    //convert json data into DOM object
+    const myJSON = JSON.stringify(t("HowItWorks.message1"));
+    const stringWithoutQuotes1 = myJSON.replace(/"/g, '');
+
+    const myJSON2 = JSON.stringify(t("HowItWorks.message2"));
+    const stringWithoutQuotes2 = myJSON2.replace(/"/g, '');
+
     return (
 
     <div id = 'aboutMe'className='about'>
-        <h1>How it works</h1>
+        <h1>What is a drawing?</h1>
+        <div className= 'lang-box-me'>
+                <Button buttonStyle="btn--outline-2" buttonSize="btn--medium" onClick={()=>handleChangeLanguage("en")}>EN</Button>
+                <Button buttonStyle="btn--outline-2" buttonSize="btn--medium" onClick={()=>handleChangeLanguage("es")}>ES</Button>
+                <Button buttonStyle="btn--outline-2" buttonSize="btn--medium" onClick={()=>handleChangeLanguage("fr")}>FR</Button>
+        </div>
         <div className='text-container'>
-            <div className= 'lang-box'>
-                <Button buttonStyle="btn--outline" buttonSize="btn--medium" onClick={()=>handleChangeLanguage("en")}>EN</Button>
-                <Button buttonStyle="btn--outline" buttonSize="btn--medium" onClick={()=>handleChangeLanguage("es")}>ES</Button>
-                <Button buttonStyle="btn--outline" buttonSize="btn--medium" onClick={()=>handleChangeLanguage("fr")}>FR</Button>
-            </div>
             <div style ={{whiteSpace: "pre-wrap"}}className='text__wrapper'>
-                <p>{t("HowItWorks.message")}</p>
+                <p dangerouslySetInnerHTML = {{__html: stringWithoutQuotes1}}></p>
+                <div class = "row">
+                    <div class = "column">
+                        <img src="Dibujos/no-knot.jpg" alt="Knot" style={{width:"80%"}}/>
+                        <figcaption class = "first-image" style={{fontSize:"15px"}}> <em>Figure 1</em> </figcaption>
+                    </div>
+                    <div class = "column">
+                        <img src="Dibujos/knot.jpg" alt="Not" style={{width:"85%"}}/>
+                        <figcaption style={{fontSize:"15px"}}> <em>Figure 2</em> </figcaption>
+                    </div>
+                </div>
+                <p dangerouslySetInnerHTML = {{__html: stringWithoutQuotes2}}></p>
             </div>
         </div>
     </div>
